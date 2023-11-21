b349dbd2da5b184b4c6a051110f0fc9dc1e9cb4d
veera776127 <veeramangalamthulasiram@gmail.com>
2023-11-21 13:45:19 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List any dependencies your package may have
+    ],
+    # Other metadata like author, author_email, description, etc.
+)
